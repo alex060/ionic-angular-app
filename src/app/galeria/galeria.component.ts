18b13eaa90b4087f87ea37256c830b67bca8a7a6
alex060 <joseidi2019@gmail.com>
2026-09-21 13:40:35 +0200

    chore: initial commit con estructura Ionic y configuracion segura de entorno

---
+++ src/app/galeria/galeria.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular';
+
+@Component({
+  selector: 'app-galeria',
+  templateUrl: './galeria.component.html',
+  styleUrls: ['./galeria.component.scss'],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+})
+export class GaleriaComponent implements OnInit {
+  // Tipado estricto recomendado
+  public tituloSeccion: string = 'Galería de Imágenes Dinámica';
+  public descripcion: string = 'Esta imagen se carga desde el directorio de assets mediante Property Binding.';
+  public rutaImagenLocal: string = 'assets/foto1.jpg';
+
+  constructor() {}
+
+  ngOnInit() {}
+}
+
